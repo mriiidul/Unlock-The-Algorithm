1a7d6f4f42b1ae0cbfc5a2266efbd0ff88f7f726
Maruf Ahammed Mridul <67202661+mriiidul@users.noreply.github.com>
2024-05-17 16:32:40 +0600

    Create Top Scorer.cpp

---
+++ Top Scorer.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int messi, ron, gm, gr, a;
+    messi = 756;
+    ron = 801;
+    gm = 34;
+    gr = 36;
+    cin >> a;
+    int goals = ron - messi + 1;
+    int required = (goals) / 2;
+    cout << required << endl;
+}
